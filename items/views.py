c4817b88e0c554d313abb49fb08e2258cb34c137
Denniel <sadiandenniel@gmail.com>
2020-07-11 15:11:05 +0800

    Add extra context

--- items/views.py
+++ items/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'items/home.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user'] = self.request.user
+        return super().get_context_data(**kwargs)
