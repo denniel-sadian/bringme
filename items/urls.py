e0aefd6da1f8d86ca39b64f44cc51b2766ee6b1c
Denniel <sadiandenniel@gmail.com>
2020-07-11 20:22:36 +0800

    Add a url for closing items

--- items/urls.py
+++ items/urls.py
@@ -7,6 +7,7 @@ app_name = 'items'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('items/', views.ItemsListView.as_view(), name='items-list'),
+    path('items/<int:pk>/', views.ItemCloseToggleRedirectView.as_view(), name='item-close-toggle'),
     path('items/<int:pk>/', views.ItemDetailView.as_view(), name='item-detail'),
     path('items/update/<int:pk>/', views.ItemUpdateView.as_view(), name='item-update'),
     path('items/delete/<int:pk>/', views.ItemDeleteView.as_view(), name='item-delete'),
