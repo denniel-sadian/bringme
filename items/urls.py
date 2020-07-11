2b2409c38dd8f0bee8d2121556911b0195972c3b
Denniel <sadiandenniel@gmail.com>
2020-07-11 15:47:14 +0800

    Add the url for the details of items

--- items/urls.py
+++ items/urls.py
@@ -7,5 +7,6 @@ app_name = 'items'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('items/', views.ItemsListView.as_view(), name='items-list'),
+    path('items/detail/<int:pk>/', views.ItemDetailView.as_view(), name='item-detail'),
     path('items/create/', views.ItemCreateView.as_view(), name='item-create')
 ]
