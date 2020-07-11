ef1db3a45ffdd762663524129e97a9912769837d
Denniel <sadiandenniel@gmail.com>
2020-07-11 15:25:42 +0800

    Create `ItemListView`

--- items/views.py
+++ items/views.py
@@ -1,5 +1,17 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+from django.urls import reverse_lazy
+
+from .models import Item
 
 
 class HomeView(TemplateView):
     template_name = 'items/home.html'
+
+
+class ItemsListView(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('accounts:login')
+
+    def get_queryset(self):
+        return Item.objects.filter(delivered=False)
