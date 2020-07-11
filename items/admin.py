dac2b115e8e4c1c0be9653c296dedc287c1ea52d
Denniel <sadiandenniel@gmail.com>
2020-07-11 13:36:21 +0800

    Register the `Item` model

--- items/admin.py
+++ items/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Item
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'closed', 'closed_by', 'delivered', 'date')
+
+
+admin.site.register(Item, ItemAdmin)
