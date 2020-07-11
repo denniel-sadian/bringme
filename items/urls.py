3f47e6da273b55402615a563f797a452c2ddf2c8
Denniel <sadiandenniel@gmail.com>
2020-07-11 21:21:53 +0800

    Add the url for marking items delivered

--- items/urls.py
+++ items/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('items/', views.ItemsListView.as_view(), name='items-list'),
     path('items/<int:pk>/close/', views.ItemCloseToggleRedirectView.as_view(), name='item-close-toggle'),
+    path('items/<int:pk>/delivered/', views.ItemMarkDeliveredView.as_view(), name='item-delivered'),
     path('items/<int:pk>/', views.ItemDetailView.as_view(), name='item-detail'),
     path('items/update/<int:pk>/', views.ItemUpdateView.as_view(), name='item-update'),
     path('items/delete/<int:pk>/', views.ItemDeleteView.as_view(), name='item-delete'),
