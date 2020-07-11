5e583fb385e371bbace6572b8c0b576cddfae2d0
Denniel <sadiandenniel@gmail.com>
2020-07-11 15:36:08 +0800

    Add a new url for the item create view

--- items/urls.py
+++ items/urls.py
@@ -6,5 +6,6 @@ app_name = 'items'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('items/', views.ItemsListView.as_view(), name='items-list')
+    path('items/', views.ItemsListView.as_view(), name='items-list'),
+    path('items/create/', views.ItemCreateView.as_view(), name='item-create')
 ]
