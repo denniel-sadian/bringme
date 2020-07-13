a4f5d3e0e59e6f9030075d83ed746f4dd94fb944
Denniel <sadiandenniel@gmail.com>
2020-07-13 15:42:24 +0800

    Do it like this

--- bringme/urls.py
+++ bringme/urls.py
@@ -22,7 +22,4 @@ urlpatterns = [
     path('', include('items.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls)
-]
-
-if settings.DEBUG:
-    urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
