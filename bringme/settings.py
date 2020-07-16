9a06f23cbfe846c0dd3b87ec2af5eac4d4c970f4
Denniel <sadiandenniel@gmail.com>
2020-07-16 14:29:51 +0800

    Change

--- bringme/settings.py
+++ bringme/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
