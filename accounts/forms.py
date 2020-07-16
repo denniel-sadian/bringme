a084880414f1f049296e58fc61ae9dd725460cbd
Denniel <sadiandenniel@gmail.com>
2020-07-16 11:10:32 +0800

    Subclass django-registration's form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django_registration.forms import RegistrationForm
 
 from .models import CustomUser
+
+
+class CustomUserForm(RegistrationForm):
+    
+    class Meta(RegistrationForm.Meta):
+        model = CustomUser
+        fields = ('email', 'name', 'address', 'contact_number', 'photo')
 
 
 class CustomUserCreationForm(UserCreationForm):
