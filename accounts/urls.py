5fa36c9a89274564c70c967c47cde231ae0b2d51
Denniel <sadiandenniel@gmail.com>
2020-07-16 12:55:57 +0800

    Use the new view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,19 +1,11 @@
 from django.conf.urls import include
 from django.urls import path
 
-from django_registration.backends.activation.views import RegistrationView
-
 from . import views
-from .forms import CustomUserForm
 
 urlpatterns = [
     path('', include("django.contrib.auth.urls")),
-    path('register/',
-        RegistrationView.as_view(
-            form_class=CustomUserForm
-        ),
-        name='django_registration_register',
-    ),
+    path('register/', views.UserRegisterView.as_view(), name='django_registration_register'),
     path('', include('django_registration.backends.activation.urls')),
     path('profile-update/', views.UpdateUserView.as_view(), name='profile-update')
 ]
