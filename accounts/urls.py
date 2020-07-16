00a2e8685519f14bd8a6610dee08673d8152fd93
Denniel <sadiandenniel@gmail.com>
2020-07-16 11:48:55 +0800

    Update this

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,19 @@
 from django.conf.urls import include
 from django.urls import path
 
-from . import views
+from django_registration.backends.activation.views import RegistrationView
 
+from . import views
+from .forms import CustomUserForm
 
 urlpatterns = [
     path('', include("django.contrib.auth.urls")),
+    path('register/',
+        RegistrationView.as_view(
+            form_class=CustomUserForm
+        ),
+        name='django_registration_register',
+    ),
     path('', include('django_registration.backends.activation.urls')),
     path('profile-update/', views.UpdateUserView.as_view(), name='profile-update')
 ]
