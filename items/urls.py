05364ceeb31b58dc22e445796e3cb1d4e902d603
Denniel <sadiandenniel@gmail.com>
2020-07-17 09:57:09 +0800

    Add a url for the about page

--- items/urls.py
+++ items/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('about/', views.AboutView.as_view(), name='about'),
     path('items/', views.ItemsListView.as_view(), name='items-list'),
     path('items/<int:pk>/close/', views.ItemCloseToggleRedirectView.as_view(), name='item-close-toggle'),
     path('items/<int:pk>/delivered/', views.ItemMarkDeliveredView.as_view(), name='item-delivered'),
