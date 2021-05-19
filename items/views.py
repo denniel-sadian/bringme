2f954dd5d9290bce2a046f1602a7efdd786d5b9e
Denniel <sadiandenniel@gmail.com>
2021-05-19 15:55:19 +0800

    Simplify increment

--- items/views.py
+++ items/views.py
@@ -173,7 +173,7 @@ class ItemMarkDeliveredView(LoginRequiredMixin, SingleObjectMixin, View):
     def post(self, request, *args, **kwargs):
         item = self.get_object()
         item.delivered = True
-        item.closed_by.deliveries = item.closed_by.deliveries + 1
+        item.closed_by.deliveries += 1
         item.closed_by.save()
         item.save()
         return redirect(reverse_lazy('items-list'))
