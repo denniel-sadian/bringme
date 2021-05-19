9e44c5e00371eb3e21c2e15eedd1d0294c38aac3
Denniel <sadiandenniel@gmail.com>
2021-05-19 14:55:21 +0800

    Format this file and add `PasswordResetView`

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,32 @@
 from django.conf.urls import include
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    path('', include("django.contrib.auth.urls")),
-    path('register/', views.UserRegisterView.as_view(), name='django_registration_register'),
-    path('', include('django_registration.backends.activation.urls')),
-    path('profile-update/', views.UpdateUserView.as_view(), name='profile-update')
+    path(
+        'password_reset/',
+        views.PasswordResetView.as_view(), name='password_reset'),
+    path(
+        '', include("django.contrib.auth.urls")
+    ),
+    path(
+        'register/',
+        views.UserRegisterView.as_view(),
+        name='django_registration_register'),
+    path(
+        'activate/<str:activation_key>/',
+        views.AccountActivationView.as_view(),
+        name='django_registration_activate',
+    ),
+    path(
+        '',
+        include('django_registration.backends.activation.urls')
+    ),
+    path(
+        'profile-update/',
+        views.UpdateUserView.as_view(),
+        name='profile-update'),
+    path('testing/', TemplateView.as_view(template_name='accounts/test.html'))
 ]
