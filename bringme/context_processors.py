0e8355bdef628ab614d55de47a0237bbb7fcab6f
Denniel <sadiandenniel@gmail.com>
2021-05-19 13:08:52 +0800

    Update this file

--- bringme/context_processors.py
+++ bringme/context_processors.py
@@ -1,8 +1,11 @@
+from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
 
 
 def others(request):
     site = get_current_site(request)
     return {
-        'domain': site.domain
+        'domain': site.domain,
+        'protocol': 'http{}'.format('s' if settings.USE_HTTPS else ''),
+        'STATIC_PREFIX': 'http{}://{}'.format('s' if settings.USE_HTTPS else '', site.domain)
     }
