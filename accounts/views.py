b249fe158ca48b3ec46298197492d2edba36e8f5
Denniel <sadiandenniel@gmail.com>
2021-05-19 14:55:59 +0800

    Add imports

--- accounts/views.py
+++ accounts/views.py
@@ -2,14 +2,18 @@ from django.views.generic.edit import CreateView
 from django.views.generic import View
 from django.views.generic.edit import UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordResetView as PassResetView
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 
 from django_registration.backends.activation.views import RegistrationView
+from django_registration.backends.activation.views import ActivationView
 
 from .forms import CustomUserCreationForm
 from .forms import CustomUserChangeForm
 from .forms import CustomUserForm
 from .models import CustomUser
+from .emails import ActivationEmail
 
 
 class UserRegisterView(RegistrationView):
