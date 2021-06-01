dc11169df2dd1dfdfc00114b14a25714f4ab0964
Denniel <sadiandenniel@gmail.com>
2021-06-01 12:17:58 +0800

    Break long lines

--- items/urls.py
+++ items/urls.py
@@ -4,12 +4,25 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    
     path('about/', views.AboutView.as_view(), name='about'),
+    
     path('items/', views.ItemsListView.as_view(), name='items-list'),
-    path('items/<int:pk>/close/', views.ItemCloseToggleRedirectView.as_view(), name='item-close-toggle'),
-    path('items/<int:pk>/delivered/', views.ItemMarkDeliveredView.as_view(), name='item-delivered'),
+    
+    path('items/<int:pk>/close/',
+         views.ItemCloseToggleRedirectView.as_view(),
+         name='item-close-toggle'),
+    
+    path('items/<int:pk>/delivered/',
+         views.ItemMarkDeliveredView.as_view(), name='item-delivered'),
+    
     path('items/<int:pk>/', views.ItemDetailView.as_view(), name='item-detail'),
-    path('items/update/<int:pk>/', views.ItemUpdateView.as_view(), name='item-update'),
-    path('items/delete/<int:pk>/', views.ItemDeleteView.as_view(), name='item-delete'),
+    
+    path('items/update/<int:pk>/',
+         views.ItemUpdateView.as_view(), name='item-update'),
+    
+    path('items/delete/<int:pk>/',
+         views.ItemDeleteView.as_view(), name='item-delete'),
+    
     path('items/create/', views.ItemCreateView.as_view(), name='item-create')
 ]
