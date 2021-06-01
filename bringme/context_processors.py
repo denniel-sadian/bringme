072cad18f94ac482df9e9ff94a88054b66fb802c
Denniel <sadiandenniel@gmail.com>
2021-06-01 12:21:02 +0800

    Add a doc string here

--- bringme/context_processors.py
+++ bringme/context_processors.py
@@ -3,6 +3,7 @@ from django.contrib.sites.shortcuts import get_current_site
 
 
 def others(request):
+    """Adds more context."""
     site = get_current_site(request)
     return {
         'domain': site.domain,
